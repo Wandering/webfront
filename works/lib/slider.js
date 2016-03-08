11e82a2b8db0301d74ad233c46888d7a4abc235f
yyp <wuhun0301@qq.com>
2016-03-08 14:20:47 +0800

    初始化AngularJS

--- works/lib/slider.js
+++ works/lib/slider.js
@@ -4,8 +4,9 @@
 app.controller('NewsController', ['$scope', function($scope,$http) {
     $scope.person = { name: "Ari Lerner" };
     ajaxFun(JSON.parse(localStorage.getItem("urlConfig")).news, "GET", {},false, function(res){
-        $scope.tests=res.bizData.rows;
-        console.log(res.bizData.rows);
-        console.log("$scope.test="+$scope.tests);
+            $scope.tests=res.bizData.rows;
+        //console.log(res.bizData.rows);
+        //console.log("$scope.test="+$scope.tests);
     });
+
 }]);
